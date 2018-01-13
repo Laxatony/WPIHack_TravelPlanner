777997a890db093a5b5cafe60a7b396d0f4242c6
ADMIN\yhsieh2 <tony.ychsieh@gmail.com>
2018-01-13 15:32:24 -0500

    Initial

---
+++ js/TravelObjects.js
@@ -0,0 +1,39 @@
+class BlockUnit {
+    constructor(blockType) {
+        this.blockType = blockType
+        this.id_timestamp = new Date().getTime();
+        this.time_Start = "";
+        this.time_End = "";
+        this.detail = "";
+    }
+
+    getTimeStamp() {
+        return this.id_timestamp;
+    }
+
+    getBlockType() {
+        return this.blockType;
+    }
+}
+
+class Spot extends BlockUnit {
+    constructor(){
+        super("spot"); //don't forget "super"
+
+        this.dayIndex = 0;
+        this.orderIndex = 0;
+
+        this.name = "";
+        this.location = "";
+        this.link = "";
+    }
+}
+
+class Transport extends BlockUnit {
+    constructor(){
+        super("transport"); //don't forget "super"
+        
+        this.location_Start = "";
+        this.location_End = "";
+    }
+}
